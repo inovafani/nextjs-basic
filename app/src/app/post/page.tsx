eda14c2cc38ae3a0abf06cbad0637369ff2a6bad
Inov Afani <inoveka@gmail.com>
2026-05-17 15:59:45 +0700

    posts berhasil di-fetch

--- app/src/app/post/page.tsx
+++ app/src/app/post/page.tsx
@@ -7,9 +7,21 @@ export interface IPost {
 export default async function PostPage(){
 
     const resp = await fetch("http://localhost:3002/posts")
+    const posts: IPost[] = await resp.json()
+
     return (
         <div>
             <h1>PostPage</h1>
+
+            <ul>
+                {
+                    posts.map(post => {
+                        return (
+                            <li key={post.id}>{post.title}</li>
+                        )
+                    })
+                }
+            </ul>
         </div>
     )
 }
